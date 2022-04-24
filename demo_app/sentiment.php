15eeab55cbe35f73109e92d3c4d7b908bdb896e4
Jonathan Suryadiputra <jsuryadiputra@gmail.com>
2022-04-24 12:43:21 +0100

    Updated and made few small changes to the response submission component, preparing to update on GCP App Engine

---
+++ demo_app/sentiment.php
@@ -0,0 +1,31 @@
+<?php
+function getSentiment($text) {
+    $response = file_get_contents('https://sentiment-ykmvrmi3zq-nw.a.run.app/analyse?text=' . urlencode($text));
+    $json = json_decode($response);
+    $sentiment_weight = $json->{'sentiment'};
+    return $sentiment_weight;
+}
+
+function buildOutcomePage($reviewText, $sentiment) {
+    echo "<html>
+        <head>
+            <link rel='stylesheet' href='style.css'>
+            <link rel='icon' type='image/png' href='favicon.png'>
+        </head>
+        <body>
+            <div class='outcomecontainer'>
+                <h1>Sentiment result</h1>
+                <h2>Review text:</h2>
+                <p style='width: 50%; text-align: center; text-justify: inter-word; line-height: 1.5;'>" . $reviewText . "</p>
+                <h3>Sentiment weighting: " . $sentiment . "</h3>
+            </div>
+        </body>
+    </html>";
+}
+
+if(isset($_POST['review'])) {
+    $reviewText = $_POST['review'];
+    $sentiment = getSentiment($reviewText);
+    buildOutcomePage($reviewText, $sentiment);
+}
+?>
